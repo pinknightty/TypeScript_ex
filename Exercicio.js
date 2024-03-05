dd80ac2c1f924cf0a29c703871c4bc61868a6549
Rafael Rosa <rafagrosa@gmail.com>
2024-03-05 20:51:01 -0300

    Arquivos addicionados

---
+++ Exercicio.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicaNumeros(a, b) {
+    return a * b;
+}
+
+function saudacao(c) {
+    c = "Rafael";
+    console.log("Olá" + c);
+}
+console.log(saudacao);
---
+++ Exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicaNumeros(a: number, b:number) {
+    return a * b;
+}
+
+function saudacao(c:string):void {
+    c = "Rafael";   
+    console.log("Olá" + c); 
+}
+
+
+console.log(saudacao);
